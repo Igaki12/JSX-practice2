4843d416b5bc192ddfb7babfe1d3e3c8a1795fdc
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-01-04 11:28:09 +0900

    Added details

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { useState } from "react";
 import "./styles.css";
 
-const names = ["Aoki", "Yamada", "Murakami", "Okugawa"];
+const names = [
+  { name: "Aoki", btns: 1 },
+  { name: "Yamada", btns: 3 },
+  { name: "Murakami", btns: 6 },
+  { name: "Okugawa", btns: 5 }
+];
 const CounterText = (props) => (
   <p>
     {props.name}のカウント数：{props.count}
   </p>
 );
+const InputBtns = (props) => {
+  return (
+    <>
+      <p>目標回数:{props.btns}回</p>
+      <input type="radio"></input>
+      <input type="radio"></input>
+      <input type="radio"></input>
+    </>
+  );
+};
 const INITIAL_COUNT = 0;
 const Counter = (props) => {
   const [count, setCount] = useState(INITIAL_COUNT);
-  const countAdd = () => setCount((prevCount) => prevCount + 1);
-  const countSub = () => setCount((prevCount) => prevCount - 1);
+  let countAdd = () => setCount((prevCount) => prevCount + 1);
+  let countSub = () => setCount((prevCount) => prevCount - 1);
   const countReset = () => setCount(INITIAL_COUNT);
   return (
     <>
@@ -19,12 +34,16 @@ const Counter = (props) => {
       <button onClick={countSub}>ボタン -1</button>
       <button onClick={countReset}>リセット</button>
       <CounterText count={count} name={props.name} />
+      <InputBtns btns={props.btns} />
+      <br />
     </>
   );
 };
 
 const ListItems = () => {
-  const items = names.map((name) => <Counter name={name} />);
+  const items = names.map((item) => (
+    <Counter name={item.name} btns={item.btns} />
+  ));
   return <>{items}</>;
 };
 
