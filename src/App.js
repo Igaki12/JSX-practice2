af2fd82da4e1f4c3f849d93007964dbfccc9186e
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-01-04 10:17:37 +0900

    Wrote Counter system

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import "./styles.css";
 
-const App = () => {
-  return <div className="App">準備完了！</div>;
+const names = ["Aoki", "Yamada", "Murakami", "Okugawa"];
+const CounterText = (props) => (
+  <p>
+    {props.name}のカウント数：{props.count}
+  </p>
+);
+const INITIAL_COUNT = 0;
+const Counter = (props) => {
+  const [count, setCount] = useState(INITIAL_COUNT);
+  const countAdd = () => setCount((prevCount) => prevCount + 1);
+  const countSub = () => setCount((prevCount) => prevCount - 1);
+  const countReset = () => setCount(INITIAL_COUNT);
+  return (
+    <>
+      <button onClick={countAdd}>ボタン +1</button>
+      <button onClick={countSub}>ボタン -1</button>
+      <button onClick={countReset}>リセット</button>
+      <CounterText count={count} name={props.name} />
+    </>
+  );
 };
 
-export default App;
+const ListItems = () => {
+  const items = names.map((name) => <Counter name={name} />);
+  return <>{items}</>;
+};
+
+export default function App() {
+  return <ListItems />;
+}
